357cd29d99c3db7a93bdb1f7ac99aa2e79c97175
Lucas Bueno <buenolucasbr@gmail.com>
2021-03-02 22:31:08 -0300

    fix button color name

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,7 +4,7 @@ export type ButtonProps = {
   children?: React.ReactNode
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
-  color?: 'blood' | 'groundAndBlood' | 'boot'
+  color?: 'blood' | 'groundAndBlood' | 'bootSole'
   outlined?: boolean
   onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
 }
